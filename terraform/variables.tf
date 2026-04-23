08d5a74fc188620a28d4fc8f41d636b352087ab8
Aryan Yadav <aryan.yadav2024@nst.rishihood.edu.in>
2026-04-23 14:30:00 +0530

    chore: initialize terraform variables

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS Region to deploy to"
+  default     = "us-east-1"
+}
+
+variable "app_name" {
+  description = "Name of the application"
+  default     = "shopsmart"
+}
